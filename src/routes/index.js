6c85d4bc5918aaae2b5b69b09f90e90d0df5351e
SantiagoLE <sle.0394@hotmail.com>
2023-09-12 21:56:43 -0500

    borrado base de datos

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routerUser = require('./user.router');
+const routerCategory = require('./category.router');
+const routerProduct = require('./product.router');
+const routerCart = require('./cart.router');
+const routerPurchase = require('./purchase.router');
+const routerProductImg = require('./productImg.router');
+const router = express.Router();
+const verifyJWT = require("../utils/verifyJWT")
+
+
+// colocar las rutas aquí
+router.use("/users", routerUser)
+router.use("/categories", routerCategory)
+router.use("/products", routerProduct)
+router.use("/cart", verifyJWT, routerCart)
+router.use("/purchase", verifyJWT, routerPurchase)
+router.use("/product_images", verifyJWT, routerProductImg)
+
+
+
+module.exports = router;
---
+++ src/tests/test_migrate.js
@@ -0,0 +1,24 @@
+require ("../models")
+require('../models/Category');
+const sequelize = require('../utils/connection');
+const user = require('./createData/user');
+
+
+// importar index models y modelos sin relaciones
+// require("../models")
+
+
+const main = async() => {
+ try{
+await sequelize.sync({ force: true });
+
+await user()
+
+console.log("Me ejecute")
+
+  process.exit();
+ } catch(error){
+       console.log(error);
+ }
+}
+main();
